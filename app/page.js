5e4689a934cdea8b30a0c9e24e84bb28fc44071b
Alekzander Gottselig <alekz.gottselig@gmail.com>
2025-12-02 21:50:07 -0700

    Week 3 assignment added
    Added the files for the week 3 assignment

--- app/page.js
+++ app/page.js
@@ -3,9 +3,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      
-      <Link href="/week-2">Week 2</Link>
+      <h1 className="text-lg font-bold m-4">CPRG 306: Web Development 2 - Assignments</h1>
+
+      <Link className="m-4" href="/week-2">Week 2 Assignment</Link><br />
+      <Link className="m-4" href="/week-3">Week 3 Assignment</Link>
     </main>
   );
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+    return (
+        <main>
+            <h1 className="text-3xl font-bold m-4">Shopping List</h1>
+            <ItemList />
+        </main>
+    );
+}
+
+export const metadata = {
+    title: "Shopping List",
+};
